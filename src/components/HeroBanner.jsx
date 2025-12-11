0137ce9afedf4721d3ea6ede5e78059560058f13
Jojo-576 <joelmarfo1@gmail.com>
2025-12-11 12:56:35 +0000

    Update HeroBanner.jsx

--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -3,17 +3,19 @@ import "./HeroBanner.css";
 
 function HeroBanner() {
   return (
-   <div className="hero-banner">
-  <h1 className="hero-title">DONKOMI SALES</h1>
-  <p className="glitch-text">50% Off AKTIVE</p>
-  <p className="call-now">CALL NOW: 0595948875</p>
-  <button className="shop-button" onClick={() => alert('NYA ABOTER3 BORGA WILL COME SOON!')}>SHOP HERE</button>
- 
-
-
- 
-</div>
-
+    <div className="hero-banner">
+      <h1 className="hero-title">DONKOMI SALES</h1>
+      <p className="glitch-text">50% Off AKTIVE</p>
+      <p className="call-now">CALL NOW: 0595948875</p>
+      <button
+        className="shop-button"
+        onClick={() =>
+          alert("NYA ABOTER3 BORGA WILL COME SOON!")
+        }
+      >
+        SHOP HERE
+      </button>
+    </div>
   );
 }
 
